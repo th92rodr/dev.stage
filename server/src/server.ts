39375f92dc0ad7468a0b72b4806425f26de1b152
th92rodr <thiago.clrodrigues@gmail.com>
2025-04-28 23:19:11 -0300

    Server: start fastify http api and add api related env variables

---
+++ server/src/server.ts
@@ -0,0 +1,39 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { env } from '@/env'
+
+const server = fastify().withTypeProvider<ZodTypeProvider>()
+
+server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler)
+
+server.register(fastifyCors, {
+  origin: env.CORS_ORIGIN,
+})
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'dev.stage',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.listen({ host: env.HOST, port: env.PORT }).then(() => {
+  console.log(`HTTP server listening at: http://${env.HOST}:${env.PORT}`)
+})
