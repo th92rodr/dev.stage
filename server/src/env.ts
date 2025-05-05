57a1c4f2ff84408adc82bc638f5525182984b32b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2025-05-05 18:29:09 -0300

    Server: configure redis db

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
 
+  REDIS_URL: z.string().url(),
+
   HOST: z.string().default('localhost'),
   PORT: z.coerce.number().default(3333),
   CORS_ORIGIN: z.string(),
