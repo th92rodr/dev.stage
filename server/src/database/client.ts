a07ffc21fdd41681e040bc125d439d0fd540718b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2025-05-05 18:03:46 -0300

    Server: create subscriptions db table migration

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,5 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
+import { subscriptions } from '@/database/schema/subscriptions'
 import { env } from '@/env'
 
 export const pg = postgres(env.DATABASE_URL)
+export const db = drizzle(pg, {
+  schema: { subscriptions },
+})
