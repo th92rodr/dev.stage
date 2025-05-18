055b2dec9f10049ad12b5d9d6989ef023d997a5f
th92rodr <thiago.clrodrigues@gmail.com>
2025-05-18 19:05:02 -0300

    Web: create invite page with invite link, stats and ranking

---
+++ web/src/app/invite/[subscriberId]/stats.tsx
@@ -0,0 +1,46 @@
+import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+
+import {
+  getSubscribersSubscriberIdRankingClicks,
+  getSubscribersSubscriberIdRankingCount,
+  getSubscribersSubscriberIdRankingPosition,
+} from '@/http/api'
+
+interface StatsProps {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } = await getSubscribersSubscriberIdRankingClicks(subscriberId)
+  const { count: inviteCount } = await getSubscribersSubscriberIdRankingCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscribersSubscriberIdRankingPosition(subscriberId)
+
+  return (
+    <div className="grid gap-3 md:grid-cols-3">
+      <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {accessCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">Acessos ao link</span>
+        <MousePointerClick className="size-5 text-purple top-3 left-3 absolute" />
+      </div>
+
+      <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {inviteCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">Inscrições feitas</span>
+        <BadgeCheck className="size-5 text-purple top-3 left-3 absolute" />
+      </div>
+
+      <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {rankingPosition ? <>{rankingPosition}&ordm;</> : '-'}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">Posição no ranking</span>
+        <Medal className="size-5 text-purple top-3 left-3 absolute" />
+      </div>
+    </div>
+  )
+}
